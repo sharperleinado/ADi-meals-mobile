fb8f28a1ab7a03dec22f95e612e86a9d5dba7aa0
Sharper <dannyhance7420@gmail.com>
2022-10-14 13:48:00 +0100

    updated file

--- my_site/search_box/views.py
+++ my_site/search_box/views.py
@@ -1,22 +1,20 @@
-from django.http import Http404
-from django.shortcuts import render
+from asyncio.windows_events import NULL
+from tkinter.tix import FileEntry
+from django.shortcuts import render,redirect
 from food_app.models import Food,Soup
-from django.http import Http404
 
 
 # Create your views here.
 
 
 def search_box(request):
+
     if request.method == "GET":
         search_box = request.GET.get("search")
         food_box = Food.objects.filter(food_item__startswith = search_box).all()
         soup_box = Soup.objects.filter(soup_item__startswith = search_box).all()
 
-        my_dict = {'food':food_box,'soup':soup_box}
+        return render(request,'search_box/search.html',{'food':food_box,'soup':soup_box})
 
-        return render(request,'search_box/search.html',context=my_dict)
-
-    else:
-        return render(request,'search_box/search.html')
+        
    
