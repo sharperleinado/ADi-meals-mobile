a1601c2d2d6790b1732afac82bbbee2c6fcb66b4
unknown <113343203+sharperleinado@users.noreply.github.com>
2024-09-03 04:40:25 +0100

    forgot to allow host and to set debug = False

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = 'django-insecure-38w+yczvta+h61ca9jq6^&z%18)dd!g)ktznjmw)n5cw+qmhd7
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True#False
+DEBUG = False
 
-ALLOWED_HOSTS = []#['adimeals.com']
+ALLOWED_HOSTS = ['adimeals.com']
 
 
 # Application definition
